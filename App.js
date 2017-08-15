ce85dc665fe508f41cb5932a55b11ba5c47af0df
root <yohgenius@gmail.com>
2017-08-15 15:23:59 +0700

    working text with image.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
